0a0b908c36833ed107e9d6d0c855cb4304e1dd38
Bryan Apellanes <bryan@threeheadz.com>
2023-09-13 10:22:12 -0500

    touched by refactor

--- bam.configuration/DefaultConfigurationApplicationNameProvider.cs
+++ bam.configuration/DefaultConfigurationApplicationNameProvider.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Bam.Net;
 using Bam.Net.Configuration;
 
 namespace Bam.Net.Configuration
